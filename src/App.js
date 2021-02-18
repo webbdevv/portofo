40aef9c8b46c8d64ba9f8e981b1e11928fc7d85d
webbdevv <kyle.xu4@gmail.com>
2021-02-17 17:55:03 -0800

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navigation from './Navigation';
-import Main from './Main'
+import Main from './Main';
+import Description from './Description';
 function App() {
   return (
   <div>
     <Navigation/>
     <Main/>
+    <Description/>
   </div>
   );
 }
--- src/Description.js
+++ src/Description.js
@@ -4,19 +4,17 @@ import './Description.css';
 
 function Description() {
     return (
+        <div>
         <section className="background_one">
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-lg-6 order-lg-2">
-                            <div className="p-5">
-                                <img className="rounded-circle img-fluid" src="./imgs/placeholder.png"/>
-                            </div>
-                            <div className="col-lg-6 order-lg-1">
-                                <div className="p-5">
-                                    <h2 className="display-4">Who I Am</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam eget felis eget nunc. Neque egestas congue quisque egestas diam. Nec ullamcorper sit amet risus nullam eget felis eget. Leo a diam sollicitudin tempor id eu. Vel facilisis volutpat est velit egestas dui.</p>
-                                </div>
-                            </div>
+                        <div className="p-5"><img className="rounded-circle img-fluid" src="/placeholder.png"/></div>
+                    </div>
+                    <div className="col-lg-6 order-lg-1">
+                        <div className="p-5"><h2 className="display-4">Who I Am</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam eget felis eget nunc. Neque egestas congue quisque egestas diam. Nec ullamcorper sit amet risus nullam eget felis eget. Leo a diam sollicitudin tempor id eu. Vel facilisis volutpat est velit egestas dui.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,25 +23,18 @@ function Description() {
         <section className="background_two">
             <div className="container">
                 <div className="row align-items-center">
+                    <div className="col-lg-6 order-lg-2">
+                        <div className="p-5"><img className="rounded-circle img-fluid" src="/placeholder.png"/></div>
+                    </div>
                     <div className="col-lg-6 order-lg-1">
-                        <div className="p-5">
-                            <img src="./imgs/placeholder.png" className="rounded-circle img-fluid">
-                        </div>
-                        <div className="col-lg-6 order-lg-2">
-                            <div className="p-5">
-                                <h2 class="display-4">
-                                    What I Do
-                                </h2>
-                                <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                </p>
-                            </div>
+                        <div className="p-5"><h2 className="display-4">Coding for a Cause</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam eget felis eget nunc. Neque egestas congue quisque egestas diam. Nec ullamcorper sit amet risus nullam eget felis eget. Leo a diam sollicitudin tempor id eu. Vel facilisis volutpat est velit egestas dui.</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </section>
+    </div>    
     )
 }
 
